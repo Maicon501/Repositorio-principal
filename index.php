6e618acbf173db16f15bbcef0fefc8e6538820d4
hp <maicondmonteiro@gmail.com>
2024-01-23 22:57:44 -0300

    resolvido problema

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
 
-<<<<<<< HEAD
+
 echo "Hello World 456!";
 
-echo "ola udemy!";
-?>
-=======
-echo "ola mundo 123";
-
-echo "ola hcode
+echo "ola udemy e ola hcode!";
 
 ?>
->>>>>>> 6936653bf88a85a913447ef8aef659fb196ef3cd
